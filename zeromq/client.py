e16995298bf46da631dcd7fdf33ec7cc9f7e1e59
Fran Tena <tarod.net@gmail.com>
2021-10-31 20:28:16 +0100

    add json file and send it

--- zeromq/client.py
+++ zeromq/client.py
@@ -2,8 +2,10 @@
 #   Hello World client in Python
 #   Connects REQ socket to tcp://localhost:5555
 #   Sends "Hello" to server, expects "World" back
+#   31/10/2021: added json
 #
 
+import json
 import zmq
 
 context = zmq.Context()
@@ -15,9 +17,22 @@ socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
-    print("Sending request %s …" % request)
+    print("Sending request %s..." % request)
     socket.send(b"Hello")
 
     #  Get the reply.
     message = socket.recv()
     print("Received reply %s [ %s ]" % (request, message))
+
+# open json file
+file = open('./resources/books.json')
+
+# get json as python object
+data = json.dumps(json.load(file))
+
+# send json 
+print("Sending json data...")
+socket.send_string(data)
+
+# close file
+file.close()
